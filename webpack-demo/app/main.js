bf0c2ff519bd74b5dd1b093cde877c5d3ce6e297
catchOneW <1104215365@qq.com>
2017-05-08 23:02:55 +0800

    2

---
+++ webpack-demo/app/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './npm install --save-dev style-loader css-loader';
+import './main.css';//使用require导入css文件
+render(<Greeter />, document.getElementById('root'));

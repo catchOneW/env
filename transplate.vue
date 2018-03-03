ec322b532a3e2980b868c8da15d4546fee6887a7
catchOneW <kaiqiw666@gmail.com>
2018-03-03 14:50:55 +0800

    Create transplate.vue

---
+++ transplate.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="move">
+
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "../style/common.scss"; //引入
+#move {
+  height: 50px;
+  width: 50px;
+  background-color: red;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      div: null,
+      distance: 0
+    };
+  },
+  methods: {
+    move() {
+      this.div.style.left = ++this.distance + "px";
+      this.div.style.top = ++this.distance + "px";
+    }
+  },
+  mounted() {
+    this.div = document.getElementById("move");
+    setInterval(() => {
+      this.move();
+    }, 30);
+  }
+};
+</script>
